eeb77276fbeab05dbf495d9e883141cf1f2cc3c4
Izaak Schroeder <izaak.schroeder@gmail.com>
2016-10-06 23:07:09 -0700

    This should go...

--- example/index.css.js
+++ example/index.css.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   '.bar': {
     color: 'green',
--- src/postcss.webpack.config.js
+++ src/postcss.webpack.config.js
@@ -2,27 +2,17 @@ import {loader, plugin} from 'webpack-partial';
 import compose from 'lodash/fp/compose';
 import identity from 'lodash/fp/identity';
 
+import purdy from 'purdy';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 // `postcss` modules.
 import autoprefixer from 'autoprefixer';
-import precss from 'precss';
 import cssimport from 'postcss-import';
 import constants from 'postcss-require';
 
 // Regular expression used to detect what kind of files to process.
 const IS_STYLE = /\.(scss|sass|css)$/;
 const IS_CSS_JS = /\.css\.js$/;
-
-/**
- * Convert a loader string and query object into a complete loader string.
- * @param {String} loader Name of loader.
- * @param {Object} query Parameters object.
- * @returns {String} Generated loader string with query.
- */
-const pack = (loader, query) => {
-  return `${loader}?${JSON.stringify(query)}`;
-};
 
 /**
  * Generate the correct `loader` object given the parameters.
@@ -32,7 +22,7 @@ const pack = (loader, query) => {
  * @param {String} loader Loader for processing the stylesheet into CSS.
  * @returns {String} Final loader.
  */
-const loaders = ({target, external, minimize, loader}) => {
+const loaders = ({target, external, minimize, loaders, plugin}) => {
   const config = {
     modules: true,
     importLoaders: 1,
@@ -45,14 +35,25 @@ const loaders = ({target, external, minimize, loader}) => {
   const styleLoader = require.resolve('style-loader');
   if (target === 'web') {
     if (external) {
-      return ExtractTextPlugin.extract({
-        fallbackLoader: styleLoader,
-        loader: `${pack(cssLoader, config)}!${loader}`,
-      });
+      return [plugin.loader({omit: 1, remove: true}), {
+        loader: styleLoader,
+      }, {
+        loader: cssLoader,
+        query: config,
+      },
+      ...loaders];
     }
-    return `${styleLoader}!${pack(cssLoader, config)}!${loader}`;
+    return [{
+      loader: styleLoader,
+    }, {
+      loader: cssLoader,
+      query: config,
+    }, ...loaders];
   }
-  return `${pack(cssLocals, config)}!${loader}`;
+  return [{
+    loader: cssLocals,
+    query: config,
+  }, ...loaders];
 };
 
 export default ({
@@ -77,8 +78,13 @@ export default ({
         // Make webpack acknowledge imported files.
         onImport: (files) => files.forEach((dep) =>
           webpack.addDependency(dep)),
-        resolve: (id, {basedir}) =>
-          webpack.resolveSync(basedir, id),
+        resolve: (id, basedir) => {
+          return new Promise((resolve, reject) => {
+            webpack.resolve(basedir, id, (err, result) => {
+              err ? reject(err) : resolve(result);
+            });
+          });
+        },
       }),
       constants({
         require: (request, _, done) => {
@@ -104,7 +110,6 @@ export default ({
           });
         },
       }),
-      precss,
       ...(Array.isArray(options) ? options : options(webpack)),
       autoprefixer({
         browsers: ['last 2 versions'],
@@ -112,33 +117,42 @@ export default ({
     ];
   };
 
-  return compose(
+  const extractor = new ExtractTextPlugin({filename});
+
+  const result = compose(
     loader({
       test: IS_STYLE,
-      loader: loaders({
-        loader: require.resolve('postcss-loader'),
+      loaders: loaders({
+        loaders: [{
+          loader: require.resolve('postcss-loader'),
+          query: {options: postcss},
+        }],
         target,
         external,
         minimize,
+        plugin: extractor,
       }),
-      query: postcss,
     }),
     loader({
       test: IS_CSS_JS,
-      loader: loaders({
-        loader: [
-          require.resolve('postcss-loader'),
-          require.resolve('css-js-loader'),
-        ].join('!'),
+      loaders: loaders({
+        loaders: [{
+          loader: require.resolve('postcss-loader'),
+          query: {options: postcss},
+        }, {
+          loader: require.resolve('css-js-loader'),
+        }],
         target,
         external,
         minimize,
+        plugin: extractor,
       }),
-      query: postcss,
     }),
     // Some crawlers or things with Javascript disabled prefer normal CSS
     // instead of Javascript injected CSS, so this plugin allows for the
     // collection of the generated CSS into its own file.
-    external ? plugin(new ExtractTextPlugin({filename})) : identity
+    external ? plugin(extractor) : identity
   )(config);
+
+  return result;
 };
